b3ec334e265a00a3b80f8d1187d1a916b5905c87
Jeff Archer <jarcher7@yahoo.com>
2022-04-29 13:49:51 -0400

    Initial Commit

---
+++ MysteryNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MysteryNumber
+//
+//  Created by Jeff Archer on 4/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
