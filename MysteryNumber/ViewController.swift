f1788666558fb3658179b070121ad1fd82a8dd0c
Jeff Archer <jarcher7@yahoo.com>
2022-04-30 23:00:28 -0400

    Orientation Lock - portrait

--- MysteryNumber/ViewController.swift
+++ MysteryNumber/ViewController.swift
@@ -7,54 +7,101 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    let MIN : Int = 100;
-    let MAX : Int = 300;
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    let MIN : Int = 75;
+    let MAX : Int = 279;
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        number.text = "299"
-        minimum.text = String(MIN)
-        maximum.text = String(MAX)
+
+        doReset()
+
         minSwitch.isOn = false
         maxSwitch.isOn = false
+
+        minimum.delegate = self
+        maximum.delegate = self
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        ViewController.lockOrientation(.portrait, andRotateTo: .portrait)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        ViewController.lockOrientation(.all)
     }
 
     @IBAction func onNewNumberClick(_ sender: Any) {
         let num = generateNumber()
         number.text = String(num)
     }
-    
+
+    @IBAction func onResetClick(_ sender: Any) {
+        doReset()
+    }
+
     func generateNumber() -> Int {
-        var min = MIN
-        var max = MAX
+        var min : Int = MIN
+        var max : Int = MAX
         if (minSwitch.isOn) {
-            let text : String = minimum.text ?? String(MIN)
-            let minVal : Int = Int(text) ?? MIN
-            if ((minVal >= 0) && (minVal < 300)) {
-                min = minVal
-            }
-            minimum.text = String(min)
+            getLimitValue(textField: minimum, defValue: MIN, value: &min)
         }
         if (maxSwitch.isOn) {
-            let text : String = maximum.text ?? String(MAX)
-            let maxVal : Int = Int(text) ?? MAX
-            if ((maxVal >= 0) && (maxVal < 300)) {
-                max = maxVal
-            }
+            getLimitValue(textField: maximum, defValue: MAX, value: &max)
+        }
+        if (max < min) {
+            (min, max) = (max, min)
+            minimum.text = String(min)
             maximum.text = String(max)
         }
         let random = Int.random(in: min...max)
         return random;
     }
-    
+
+    fileprivate func doReset() {
+        number.text = "? ? ?"
+        minimum.text = String(MIN)
+        maximum.text = String(MAX)
+    }
+
+    fileprivate func getLimitValue(textField: UITextField, defValue: Int, value: inout Int) {
+        let text : String = textField.text ?? String(defValue)
+        let minVal : Int = Int(text) ?? defValue
+        if ((minVal >= 0) && (minVal <= 300)) {
+            value = minVal
+        } else {
+            value = defValue
+        }
+        textField.text = String(value)
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
+
     @IBOutlet weak var number: UITextField!
     @IBOutlet var newNumberButton: UIView!
     @IBOutlet weak var minSwitch: UISwitch!
     @IBOutlet weak var maxSwitch: UISwitch!
     @IBOutlet weak var minimum: UITextField!
     @IBOutlet weak var maximum: UITextField!
+
+    static func lockOrientation(_ orientation: UIInterfaceOrientationMask) {
+
+        if let delegate = UIApplication.shared.delegate as? AppDelegate {
+            delegate.orientationLock = orientation
+        }
+    }
+
+    /// OPTIONAL Added method to adjust lock and rotate to the desired orientation
+    static func lockOrientation(_ orientation: UIInterfaceOrientationMask, andRotateTo rotateOrientation:UIInterfaceOrientation) {
+
+        self.lockOrientation(orientation)
+
+        UIDevice.current.setValue(rotateOrientation.rawValue, forKey: "orientation")
+    }
+
 }
 
